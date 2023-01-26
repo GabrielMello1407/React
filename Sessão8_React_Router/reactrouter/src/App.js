41030ef65dc8e43cad5027fa5689abcfb3900429
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-01-26 18:35:50 -0300

    router dinamico

---
+++ Sessão8_React_Router/reactrouter/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+//1 - config react router
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+
+//Components
+import Navbar from './components/Navbar';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        {/*2- links com react router */}
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='about' element={<About/>}/>
+          {/*4- Criando nova rota dinamica */}
+          <Route path='/Products/:id' element={<Product/>}/>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
