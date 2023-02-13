88b6b05d60fffe1703bc410d0c3e081bb9bafcf6
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-02-13 14:34:13 -0300

    typescript

---
+++ Sessao15_TypeScript/index.ts
@@ -0,0 +1,12 @@
+const firstName = "Gabriel"
+const anotherName = 1
+const x = true
+
+function greeting(name:string){
+    console.log("olá " + name)
+}
+
+greeting(firstName)
+//greeting(anotherName)
+//greeting(x)
+
